bef9a3971254272e2bdcee574cf2398cd9dcc317
Hanny Paz <hwpazz@gmail.com>
2025-06-26 15:24:18 -0500

    creando el banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,7 +2,9 @@
 
 function Banner() {
   return (
-    <div>Banner</div>
+    <section id="" className="h-screen bg-[url()] bg-cover bg-center flex items-center justify-center text-center text-white px-6">
+      <div className=""></div>
+    </section>
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
+import { BarChart3, AreaChart, PieChart } from "lucide-react";
 
-import React from 'react'
 
 export default function Navbar (){
   return (
     <nav className="w-full bg-gray-400 text-white px-6 py-4 flex justify-between items-center shadow-md fixed top-0 z-50">
-        <div className="text-2xl font-bold">Hanny Wilson Paz</div>
+        <div className="flex items-center text-2xl gap-2 font-bold">
+          <PieChart className="w-6 h-6 text-blue-400"/>
+          Hanny Wilson Paz
+        </div>
         <div className="space-x-4 text-sm">
             <Link href="/">Inicio</Link>
             <Link href="#presentacion">Sobre mí</Link>
