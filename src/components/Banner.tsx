45301d985860bc47f922a5beef81761a326fca60
Hanny Paz <hwpazz@gmail.com>
2025-06-26 17:49:41 -0500

    terminando el Banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,8 +2,18 @@
 
 function Banner() {
   return (
-    <section id="" className="h-screen bg-[url()] bg-cover bg-center flex items-center justify-center text-center text-white px-6">
-      <div className=""></div>
+    <section 
+    id="inicio" 
+    className="pt-24 h-[50vh] bg-[url('/images/Banner_Datos.png')] bg-cover bg-center flex items-center justify-center text-center text-white px-6"
+    >
+      <div className="">
+        <h1 className="text-4xl md:text-5xl font-bold mb-4">
+          Hola, soy Hanny Wilson Paz
+        </h1>
+        <p className="text-lg max-w-xl">
+          +3 años de experiencia utilizando Python, Power BI, SQL y herramientas para el análisis de datos.
+        </p>
+      </div>
     </section>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,20 @@
 import Link from "next/link";
-import { BarChart3, AreaChart, PieChart } from "lucide-react";
+import { PieChart } from "lucide-react";
 
 
 export default function Navbar (){
   return (
-    <nav className="w-full bg-gray-400 text-white px-6 py-4 flex justify-between items-center shadow-md fixed top-0 z-50">
-        <div className="flex items-center text-2xl gap-2 font-bold">
-          <PieChart className="w-6 h-6 text-blue-400"/>
+    <nav className="w-full bg-color-background text-white px-6 py-4 flex justify-between items-center shadow-md top-0 z-50">
+        <Link href="/" className="flex items-center text-2xl gap-2 font-bold hover:text-amber-300">
+          <PieChart className="w-6 h-6 text-amber-300"/>
           Hanny Wilson Paz
-        </div>
-        <div className="space-x-4 text-sm">
-            <Link href="/">Inicio</Link>
-            <Link href="#presentacion">Sobre mí</Link>
-            <Link href="#proyectos">Proyectos</Link>
-            <Link href="#habilidades">Habilidades</Link>
-            <Link href="#Contacto">Contactos</Link>
+        </Link>
+        <div className="space-x-4 text-lg">
+          <Link href="/" className="hover:text-amber-300 transition-colors">Inicio</Link>
+          <Link href="#presentacion" className="hover:text-amber-300 transition-colors">Sobre mí</Link>
+          <Link href="#proyectos" className="hover:text-amber-300 transition-colors">Proyectos</Link>
+          <Link href="#habilidades" className="hover:text-amber-300 transition-colors">Habilidades</Link>
+          <Link href="#Contacto" className="hover:text-amber-300 transition-colors">Contactos</Link>
         </div>
     </nav>
   )
