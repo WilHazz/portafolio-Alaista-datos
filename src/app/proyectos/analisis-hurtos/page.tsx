15259f13afdf22009f74435fc59689e167df1a38
Hanny Wilson Paz <hwpazz@gmail.com>
2025-07-27 13:20:31 -0500

    colocando el banner para analisis de proyecto hurtos en colombia

---
+++ src/app/proyectos/analisis-hurtos/page.tsx
@@ -0,0 +1,18 @@
+
+function page(){
+    return(
+        <section className="bg-background text-centerpb-20">
+             <div className="relative ">
+         <img src="/images/Banner_Datos.png" className="h-[50vh] md:h-[60vh] w-full object-cover" alt="Análisis de ventas E-commerce" />   
+        <div className="absolute inset-0 flex items-center justify-center">
+            <h1 className="text-4xl md:text-5xl font-bold text-center px-4 text-white">
+                Análisis de Hurtos en Colombia
+            </h1>
+
+        </div>
+        </div>
+        </section>
+    )
+}
+
+export default page
