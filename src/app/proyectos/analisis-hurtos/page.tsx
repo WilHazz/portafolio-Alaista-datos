7dd35fc897f592b843d85e14be7e73cb397b6698
Hanny Wilson Paz <hwpazz@gmail.com>
2025-07-27 21:36:30 -0500

    creando la secciones para el proyecto de Hurtos

--- src/app/proyectos/analisis-hurtos/page.tsx
+++ src/app/proyectos/analisis-hurtos/page.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link"
+import { FaGithub } from "react-icons/fa" 
+
 
 function page(){
     return(
@@ -8,8 +11,123 @@ function page(){
             <h1 className="text-4xl md:text-5xl font-bold text-center px-4 text-white">
                 Análisis de Hurtos en Colombia
             </h1>
-
         </div>
+        </div>
+
+        <div className="max-w-4xl mx-auto py-12 px-6 text-center">
+            <p className="text-lg text-white">
+            Este proyecto analiza los reportes de hurtos en Colombia con datos proporcionados por el gobierno de datos libres de Colombia.
+            Se utilizaron herramientas como <strong>Python</strong>, <strong>Pandas</strong> y <strong>Plotly </strong> 
+            para la exploración y visualización de datos, y se construyó un dashboard en <strong>Look Studio </strong> 
+            para mostrar los hallazgos.
+            </p>
+        </div>
+
+         {/* Resumen Ejecutivo */}
+        <div className="w-full mx-auto text-left px-10 py-4">
+            <h2 className="text-4xl font-bold text-amber-400 mb-6">Resumen Ejecutivo</h2>
+            <p className="mb-4 text-lg text-white">
+                En Colombia, los hurtos representan uno de los delitos con mayor impacto social, pero su análisis detallado ha sido limitado por la 
+                dispersión y complejidad de los datos. Este proyecto busca comprender el comportamiento de los hurtos 
+                en el país a través de un análisis exploratorio de los registros reportados por el gobierno de datos libres de Colombia.
+            </p>
+            <p className="text-white">
+                <ul className="">
+                    <li>¿Qué grupos de edad y género son más vulnerables?</li>
+                    <li>¿En qué regiones y meses se presentan más casos?</li>
+                    <li>¿Cuál es la relación entre el uso de armas y los tipos de hurto?</li>
+                    <li>¿Existen patrones en la estacionalidad o en los métodos utilizados?</li>
+                </ul>
+        </p>
+        </div>
+
+         {/* Insights */}
+        <div className="w-full mx-auto text-left px-10 py-6">
+            <h2 className="text-2xl font-bold text-amber-400 mb-4">Objeto más robado</h2>
+            <p className="mb-6 text-lg text-white">
+                El objeto con más frecuencia  hurtado fue la <strong>motocicleta</strong>, con más de <strong>447499 registros</strong> durante el periodo analizado.
+                Este resultado evidencia la alta vulnerabilidad de este tipo de vehículo frente a los delincuentes, y sugiere la urgencia de implementar medidas de 
+                control más efectivas, como zonas seguras de parqueo, campañas de marcación y vigilancia reforzada en sectores críticos.
+            </p>
+            <div className="flex justify-center">
+                <img
+                    src="/images/Proyecto_Hurtos/Tipo_hurto.png" 
+                    alt="Producto mas vendido"
+                    className="w-full max-w-xl h-auto shadow-md"
+                />
+            </div>
+        </div>
+
+         {/* Acciones y Recomendaciones */}
+         <div className="w-full mx-auto text-left px-10 py-6">
+            <h2 className="text-2xl font-bold text-amber-400 mb-4">
+                Hallazgos Clave
+            </h2>
+            <p className="mb-6 text-lg text-white">
+                A través de visualizaciones exploratorias, se identificaron patrones clave en los hurtos reportados. Estas gráficas permiten comprender mejor la magnitud, evolución y características del delito en Colombia.
+            </p>
+            {/* Gráfica 1: Top 10 tipos de hurtos */}
+                <div className="flex flex-col md:flex-row gap-6 mb-12 items-center">
+                    <div className="flex flex-col items-center">
+                    <h3 className="text-xl font-semibold mb-2 text-white">Medida 1</h3>
+                    <img 
+                    src="/images/Proyecto_Hurtos/top10.png" 
+                    alt="Top 10 tipos de hurto" 
+                    className="w-[400px] md:w-[480px] rounded-lg shadow-md" />
+
+                    </div>
+                    <p className="text-gray-400 mt-2 text-justify">
+                        Esta gráfica muestra los 10 tipos de hurto más frecuentes, permitiendo identificar los métodos más usados por los delincuentes en Colombia.
+                    </p>
+                </div>
+                
+            <div>
+            {/* Gráfica 2: Evolución anual de hurtos */}
+            <div className="flex flex-col md:flex-row gap-6 mb-12 items-center">
+                <div className="flex flex-col items-center">
+
+                <h3 className="text-xl font-semibold mb-2 text-white">Medida 2</h3>
+
+                <img
+                    src="/images/Proyecto_Hurtos/EvolucionAnual.png"
+                    alt="Evolución anual de hurtos"
+                    className="w-[400px] md:w-[460px] rounded-lg shadow-md"
+                />
+                </div>
+                <p className="text-gray-400 md:w-1/2 text-justify">
+                    Se observa cómo ha variado el número de hurtos año tras año,
+                    evidenciando posibles aumentos o reducciones en el delito a nivel nacional.
+                </p>
+            </div>
+            {/* Gráfica 2: Evolución anual de hurtos */}
+            <div className="flex flex-col md:flex-row gap-6 mb-12 items-center">
+                <div className="flex flex-col items-center">
+
+                <h3 className="text-xl font-semibold mb-2 text-white">Medida 3</h3>
+
+                <img
+                    src="/images/Proyecto_Hurtos/armas_usadas.png"
+                    alt="Evolución anual de hurtos"
+                    className="w-[400px] md:w-[460px] rounded-lg shadow-md"
+                />
+                </div>
+                <p className="text-gray-400 md:w-1/2 text-justify">
+                   Esta visualización presenta la distribución de los hurtos según el tipo de arma empleada, 
+                   destacando la peligrosidad y modalidad de los hechos.
+                </p>
+            </div>
+
+            </div>
+         </div>
+
+         {/* Boton a Github */}
+        <div className="text-center pt-12 py-4">
+            <Link href="https://github.com/WilHazz/Analisis-Datos-Hurtos" target="_blank">
+            <button className="inline-flex items-center text-center justify-center bg-amber-400 text-black font-semibold px-6 py-3 cursor-pointer rounded-3xl crounded-lg hover:bg-amber-500 transition gap-3">
+               <FaGithub className="text-3xl"/> 
+               Ver proyecto
+            </button>
+            </Link>
         </div>
         </section>
     )
